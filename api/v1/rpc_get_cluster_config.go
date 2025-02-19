77a648b162f0e1ba55235edeea88c349b60cc0e5
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-02-19 12:53:59 +0400

    api: Add datastore and nodeTaints to GetClusterConfigRPC

--- api/v1/rpc_get_cluster_config.go
+++ api/v1/rpc_get_cluster_config.go
@@ -8,5 +8,12 @@ type GetClusterConfigRequest struct{}
 
 // GetClusterConfigResponse is the response message for the GetClusterConfig RPC.
 type GetClusterConfigResponse struct {
+	// Config is the cluster configuration.
 	Config UserFacingClusterConfig `json:"status"`
+	// Datastore is the datastore configuration.
+	Datastore UserFacingDatastoreConfig `json:"datastore,omitempty" yaml:"datastore,omitempty"`
+	// NodeTaints is a list of taints to apply to the nodes.
+	// If the request comes from a control plane node, will contain the taints applied on the control plane nodes.
+	// If the request comes from a worker node, will contain the taints applied on the worker nodes.
+	NodeTaints *[]string `json:"nodeTaints,omitempty" yaml:"nodeTaints,omitempty"`
 }
